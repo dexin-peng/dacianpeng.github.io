27792b569b46047a7629c1a681a85a9e34449a0d
dacianpeng <dacianpeng@gmail.com>
2023-02-21 15:22:37 +0800

    remove baidu tracker

--- js/baidu-analytics.js
+++
@@ -1,9 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?13459ef1165dfea135327071789ed798";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
